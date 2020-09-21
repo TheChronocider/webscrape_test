b35c0169a89e7d163c32e04dc02fe4992b3c37f3
Zane <zane.a.rogalewicz@gmail.com>
2020-09-21 11:40:31 +1000

    Added python file

--- Webscrape.py
+++ Webscrape.py
@@ -6,66 +6,26 @@
 
 # = = = = = = = = = = = = = 
 # Created:       15:09:2020
-# Last Updated:  18:09:2020
+# Last Updated:  21:09:2020
 #
 # Description:  Webscraper targeted at reading table format of fuel prices provided by AIP and preserve them in CSV format
+#               Focused on using Requests library
 # = = = = = = = = = = = = = 
 
 
-# In[192]:
+# In[15]:
 
 
-from selenium import webdriver
-import re # Importing regex
 import datetime
 import os
 import csv
 
+# Focusing on only using Requests
+import requests
 
-# In[199]:
 
+# In[16]:
 
-# Input/s:  String
-# Output/s: [String]
-#
-# Description: Splits string into sentences and seperated numeric values
-#
-def split_string(fstring):
-    # Return is string is empty
-    if len(fstring) <= 0:
-        return []
-    
-    # Check if current char is not a white space
-    if fstring[:1] != ' ':
-        # Current value found, get word remainder
-        value = split_word(fstring)
-        
-        # Return array of found value and remainder
-        return [value[0]] + split_string(value[1])
-        
-    else:
-        # No current value, continue to next
-        return split_string(fstring[1:])
-
-# Input/s:  String
-# Output/s: String, String
-#
-# Description: Splits string into sentence ending at numeric value
-#
-def split_word(fstring):
-    # Check if we've got chars left
-    if len(fstring) <= 0:
-        return '', ''
-    
-    # Check if we haven't reached the end of the word
-    if (fstring[:1] != ' ') | ((len(fstring) >= 2) & (fstring[1:2].isalpha())):
-        value = split_word(fstring[1:])
-        return fstring[:1] + value[0] , value[1]
-    
-    else:
-        return '', fstring
-    
-    return '', ''
 
 # Input/s:  String, datetime
 # Output/s: Boolean
@@ -96,23 +56,17 @@ def check_week(fFileName, fCurr_time):
     
 
 
-# In[200]:
+# In[17]:
 
-
-# Web Driver location
-path = r'.\Chromedriver'
 
 # Fuel data CSV
 fileName = "fuel_data.csv"
 
-# Fuel data location
-url = 'https://aip.com.au/pricing/new-south-walesact-retail-petrol-prices'
-
 # Target Cities
-targets = ['Canberra', 'Sydney', 'Batemans Bay', 'Cooma', 'Goulburn', 'Coffs Harbour', 'Wollongong', 'Yass']
+targets = ['canberra', 'sydney', 'batemans bay', 'cooma', 'goulburn', 'coffs harbour', 'wollongong', 'yass']
 
 
-# In[201]:
+# In[18]:
 
 
 # Get current date
@@ -128,40 +82,34 @@ curr_week = curr_day - datetime.timedelta(idx)
 curr_time = datetime.datetime(curr_week.year,curr_week.month,curr_week.day)
 
 
-# In[205]:
+# In[21]:
 
 
+# Gets skipped if we already have this weeks valeus
 if (not check_week(fileName, curr_time)):
-    # Driver opening
-    driver = webdriver.Chrome(executable_path = path)
+    ## The request made to get the raw infromation used for populating the table
+    ## Found in the Networks tab of inspect element
+    url = requests.get('https://aip.com.au/aip-api-request?api-path=public/api&call=nswUlpTable&fuelType=undefined')
 
-    #Specify wait 10 seconds
-    driver.implicitly_wait(15)
-    
-    # Executing fuel place location
-    driver.get(url)
-    
-
-    # Get values between 'tr' elements and split them as desired
-    elements = [split_string(element.text) for element in driver.find_elements_by_css_selector('tr')]
-
-    # Get desired fuel data and add timestamp and current date
-    current_prices = []
-    for element in elements:
-        if element[0] in targets:
-            current_prices = current_prices + [[curr_time.timestamp()] + [curr_time.strftime("%Y-%m-%d")] + element]
-    
-    # Write fuel prices to file
+    ## The URL is a JSON file, thus we will be just reading it as a JSON
+    data = url.json()
+        
     with open(fileName, 'a', newline='') as fd:
         writer = csv.writer(fd)
-        for element in current_prices:
-            writer.writerow(element)
-    
-    driver.close()
-
-
-# In[ ]:
-
-
-
+        ## Iterate through JSON objects
+        for item in data:
+            if data[item]['location'].lower() in targets:
+                # Following syntax
+                element = [
+                    curr_time.timestamp(),
+                    curr_time.strftime("%Y-%m-%d"),
+                    data[item]['location'],
+                    data[item]['weeklyAverage'],
+                    data[item]['weeklyChange'],
+                    data[item]['diff'],
+                    data[item]['weeklyLow'],
+                    data[item]['weeklyHigh']
+                ]
+                
+                writer.writerow(element)
 
